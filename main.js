7ba4bd6e5d3a9a9c13a629d9392ac55f230b574e
Francisco Anderson Rafael da Silva <andersonrafael@users.noreply.github.com>
2024-06-01 10:37:47 -0300

    projeto final versão 1.0

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
 const ulItens = document.getElementById("lista-de-itens")
 const ulItensComprados = document.getElementById("itens-comprados")
+const listaRecuperada = localStorage.getItem('listaDeItens')
+
+function atualizaLocalStorage() {
+    localStorage.setItem('listaDeItens', JSON.stringify(listaDeItens))
+}
+
+if (listaRecuperada) {
+    listaDeItens = JSON.parse(listaRecuperada)
+    mostrarItem()
+} else {
+    listaDeItens = []
+}
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault()
@@ -17,25 +29,25 @@ function salvarItem() {
     const comprasItem = itensInput.value
     const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase())
 
-    if(checarDuplicado) {
+    if (checarDuplicado) {
         alert("Item já existe")
     } else {
-    listaDeItens.push({
-        valor: comprasItem,
-        checar: false
-    })
-}
+        listaDeItens.push({
+            valor: comprasItem,
+            checar: false
+        })
+    }
 
     itensInput.value = ''
 }
 
-function mostrarItem(){
+function mostrarItem() {
     ulItens.innerHTML = ''
     ulItensComprados.innerHTML = ''
-    
+
     listaDeItens.forEach((elemento, index) => {
-        if(elemento.checar) {
-        ulItensComprados.innerHTML += `
+        if (elemento.checar) {
+            ulItensComprados.innerHTML += `
     <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" checked class="is-clickable" />  
@@ -47,7 +59,7 @@ function mostrarItem(){
     </li>
         `
         } else {
-        ulItens.innerHTML += `
+            ulItens.innerHTML += `
     <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" class="is-clickable" />
@@ -55,12 +67,12 @@ function mostrarItem(){
         </div>
 
         <div>
-            ${ index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
+            ${index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>
     `
-    }
+        }
     })
 
     const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
@@ -78,10 +90,10 @@ function mostrarItem(){
     deletarObjetos.forEach(i => {
         i.addEventListener('click', (evento) => {
             valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
-            listaDeItens.splice(valorDoElemento,1)
+            listaDeItens.splice(valorDoElemento, 1)
             mostrarItem()
         })
-    })    
+    })
 
     const editarItens = document.querySelectorAll(".editar")
 
@@ -90,8 +102,9 @@ function mostrarItem(){
             itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
             mostrarItem()
         })
-    })  
+    })
 
+    atualizaLocalStorage()
 }
 
 function salvarEdicao() {
